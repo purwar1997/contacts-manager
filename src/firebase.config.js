0c137a54232413b2deef326280e9a8c02a6fb270
purwar1997 <shubhampurwar511@gmail.com>
2023-05-21 15:46:44 +0530

    project initialized

---
+++ src/firebase.config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: 'contacts-manager-dba7f.firebaseapp.com',
+  projectId: 'contacts-manager-dba7f',
+  storageBucket: 'contacts-manager-dba7f.appspot.com',
+  messagingSenderId: '753006666907',
+  appId: '1:753006666907:web:064a91522c41768ebb923e',
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export default db;
